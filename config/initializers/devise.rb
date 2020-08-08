52637ba1325e2a8781e441f086f2e5bcf207ae7a
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-08-08 14:59:32 +0530

    Devise juser/email login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -48,6 +48,9 @@ Devise.setup do |config|
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [:email]
 
+  config.authentication_keys = [:login]
+  
+
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
   # find_for_authentication method and considered in your model lookup. For instance,
@@ -58,7 +61,7 @@ Devise.setup do |config|
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
   # to authenticate or find a user. Default is :email.
-  config.case_insensitive_keys = [:email]
+  config.case_insensitive_keys = [:email, :username, :login]
 
   # Configure which authentication keys should have whitespace stripped.
   # These keys will have whitespace before and after removed upon creating or
